3618b309162b8cc6e0d8f0c18f8f1272ffec8cb0
rjmcbain <rmcbain01@gmail.com>
2017-12-19 19:02:32 -0700

    I DID IT

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var movie = require("./movie");
-var threeFavoriteMovies = [];
+var threeFavoriteMovies = ['Forrest Gump', 'Fight Club', 'A Clockwork Orange'];
 threeFavoriteMovies.forEach(function(film){
-  // your code here
+  movie(film);
 });
+
+
+// 011039210358699929309:mrt7xxuynqg
+
+// AIzaSyDyqT1-HxCHIMmcBI-In2YeOaOm4gySGzw
+
+// https://www.googleapis.com/customsearch/v1?key=AIzaSyDyqT1-HxCHIMmcBI-In2YeOaOm4gySGzw&cx=011039210358699929309:mrt7xxuynqg&q=face+off+movie
---
+++ env.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+	key: "AIzaSyDyqT1-HxCHIMmcBI-In2YeOaOm4gySGzw",
+	id: "011039210358699929309:mrt7xxuynqg"
+};
